f728abca006c6fa371af85ba4f448d9dc28e802a
glaxy333 <glaxy@hipatia.net>
2014-03-24 12:23:33 +0100

    test linee lunghe

--- lezione/pattern/singleton/A.java
+++ lezione/pattern/singleton/A.java
@@ -1,3 +1,10 @@
+/*
+Il pattern singleton risolve il problema di fare in modo che una certa classe possa essere istanziata una volta sola.
+Ciò graficamente si rende con un numero 1 vicino al numero della classe.
+A livello di codice con un campo statico dello stesso tipo della classe, il costruttore privato e un metodo pubblico create che restituisce un oggetto A se è la prima volta che viene chiamato, chiama un'eccezione altrimenti o ritorna l'oggetto già creato.
+Posso fare piccole varianti dando la possibilità di creare un tot di istanze e non solo una.
+In java il pattern singleton dà la possibilità di avere l'idea di variable globale a tutto il programma, cosa che in java solitamente non esiste.
+*/
 public class A {
     private static A istance;
 
