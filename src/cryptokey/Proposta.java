5b7d711add714280ecc3fa4ebe2bbd2741b27e91
glaxy333 <glaxy@hipatia.net>
2014-05-01 03:37:21 +0200

    tante cose

---
+++ src/cryptokey/Proposta.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2014 glaxy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package cryptokey;
+
+/**
+ *
+ * @author glaxy
+ */
+public class Proposta {
+    
+    private String stato;
+    private boolean notificata;
+    private UserInfo proponente;
+    private UserInfo partner;
+    
+    /* tipo di sec? */
+    public Proposta(Studente user, Studente partner, String sec) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public Proposta(QueryResult queryResult) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public static void load(int id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public static void caricaAttiva(int idProp, int idPartner) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
---
+++ src/cryptokey/SistemaCifratura.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2014 glaxy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package cryptokey;
+
+/**
+ *
+ * @author glaxy
+ */
+public class SistemaCifratura {
+    
+    private int id;
+    private String chiave;
+    private String metodo;
+    private CalcolatoreMappatura calcolatore;
+    private Mappatura mappatura;
+    private Proposta proposta;
+    private UserInfo creatore;
+    
+    /* void? */
+    public static void caricaSistemiCifratura(Studente st) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public static SistemaCifratura load(Studente user1, Studente user2) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public SistemaCifratura(String chiave, String metodo) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    /* QueryResult? */
+    public SistemaCifratura(QueryResult queryResult) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public String prova(String testo) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public void calcolaMappatura() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public void save() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
