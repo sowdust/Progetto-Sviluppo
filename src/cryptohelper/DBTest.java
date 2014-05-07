4db0a2b8b110d2bfc0777e9a2abfc289b00e7695
Pibborn <matcerrato@gmail.com>
2014-05-07 13:54:43 +0200

    aggiunto DBController

---
+++ src/cryptohelper/DBTest.java
@@ -0,0 +1,15 @@
+
+package cryptohelper;
+
+/**
+ *
+ * @author Mattia Cerrato, mattia.cerrato[at]studenti.unito[dot]it
+ */
+public class DBTest {
+    public static void main(String[] args) {
+        String out = "";
+        DBController db = DBController.getInstance();
+        String result = db.execute("select * from CRYPTOUSER.USERS");
+        System.out.println(result);
+    }
+}
