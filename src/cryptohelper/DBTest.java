5186354a7808acd049d27bde9621d8e140bab3ca
Pibborn <matcerrato@gmail.com>
2014-05-08 14:27:55 +0200

    inizio branch, fix SQLException

--- src/cryptohelper/DBTest.java
+++ src/cryptohelper/DBTest.java
@@ -1,6 +1,8 @@
 
 package cryptohelper;
 
+import java.sql.SQLException;
+
 /**
  *
  * @author Mattia Cerrato, mattia.cerrato[at]studenti.unito[dot]it
@@ -8,7 +10,11 @@ package cryptohelper;
 public class DBTest {
     public static void main(String[] args) {
         String out = "";
-        DBController db = DBController.getInstance();
+        try {
+            DBController db = DBController.getInstance();
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
         //String result = db.execute("select * from CRYPTO_USER.USERS");
         //System.out.println(result);
     }
