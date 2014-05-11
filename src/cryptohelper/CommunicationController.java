3c30f481e5d4585ae685111e51bf8c99b2bc1d3e
Pibborn <matcerrato@gmail.com>
2014-05-11 13:54:28 +0200

    cominciato DSD, manca testing

--- src/cryptohelper/CommunicationController.java
+++ src/cryptohelper/CommunicationController.java
@@ -3,6 +3,8 @@ package cryptohelper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * CommunicationController è il Controller delle comunicazioni tra le classi che si occupano
@@ -11,6 +13,10 @@ import java.sql.SQLException;
  * @author Mattia Cerrato, mattia.cerrato[at]studenti.unito[dot]it
  */
 public class CommunicationController {
+    /* NOTE:
+    - La query di getDestinatari è leggermente diversa da quella indicata nel DSD: non mi sembra che
+    se voglio i DESTINATARI delle proposte di un certo utente io voglia anche l'utente stesso.
+    */
     public static CommunicationController instance = null;
     
     private CommunicationController() {}
@@ -29,7 +35,7 @@ public class CommunicationController {
         return m;
     }
     
-    public ResultSet inviaDecisione(Proposta proposta, String decisione) throws SQLException{
+    public ResultSet inviaDecisione(Proposta proposta, String decisione) throws SQLException {
         if(decisione.compareTo("accettata") == 0) {
             Proposta old = Proposta.caricaAttiva(proposta.getProponente().getId(), proposta.getPartner().getId());
             if (old != null) {
@@ -42,4 +48,18 @@ public class CommunicationController {
         return rs2;
     }
     
+    public List<UserInfo> getDestinatari(Studente st) throws SQLException {
+        DBController dbc = DBController.getInstance();
+        int id = st.getId();
+        ResultSet rs = dbc.execute("SELECT Studente.ID, nome, cognome FROM crypto_user.Proposta"
+                                 + "JOIN crypto_user.STUDENTE"
+                                 + "ON crypto_user.Proposta.PROPONENTE = "+st.getId()+" AND crypto_user.PROPOSTA.STATO = 'accepted'" 
+                                 + "AND crypto_user.STUDENTE.ID != "+st.getId());
+        ArrayList result = new ArrayList<UserInfo>();
+        while(rs.next()) {
+            UserInfo us = new UserInfo(rs.getInt("id"), rs.getString("nome"), rs.getString("cognome"));
+            result.add(us);
+        }
+        return result;
+    }
 }
