41cf8b94479c167818e3c54c804a10e3a0c3dbae
sowdust <sowdust@autistici.org>
2014-05-23 14:58:18 +0200

    commit di sicurezza prima di modificare mappaturaP.conflitto

--- src/cryptohelper/model/Ipotesi.java
+++ src/cryptohelper/model/Ipotesi.java
@@ -16,60 +16,71 @@ import java.util.List;
  */
 public class Ipotesi {
 
-        public MappaturaParziale map;
-        public List<Ipotesi> figli;
-        public Ipotesi padre;
-        public String commento;
-        
-        public Ipotesi(MappaturaParziale map, Ipotesi padre) {
-            this.padre = padre;
-            this.map = map;
-            this.figli = new LinkedList<Ipotesi>();
-        }
-        
-        public Ipotesi aggiungiIpotesi(MappaturaParziale map) {
-            Ipotesi ip = new Ipotesi(map, this);
-            figli.add(ip);
-            return ip;
-        }
-        
-        public MappaturaParziale getStato() {
-            
-            if( null == padre) {
-                return new MappaturaParziale(map);
-            }
-            return map.merge(padre.getStato());
-        }
-        
-        public Ipotesi trovaConflitto(MappaturaParziale m) {
-            if(map.conflitto(m)) {
-                return this;
-            }
-            if(null == padre) {
-                return null;
-            }
-            return padre.trovaConflitto(m);
-        }
-    
-        public void stampa(int d, Ipotesi ipotesiCorrente) {
-            System.out.print("  ");
-            for(int i = 0; i <= d; ++i) {
-                System.out.print("     ");
-            }
-            if(this == ipotesiCorrente) {
-                System.out.print("\b\b**");
-               
-            }
-            System.out.println(map);
+    public MappaturaParziale map;
+    public List<Ipotesi> figli;
+    public Ipotesi padre;
+    public String commento;
 
-            for(Ipotesi i : figli) {
-                if(i!= null) {
-                    i.stampa(d + 1, ipotesiCorrente);
-                }
+    public Ipotesi(MappaturaParziale map, Ipotesi padre) {
+        this.padre = padre;
+        this.map = map;
+        this.figli = new LinkedList<Ipotesi>();
+    }
+
+    public Ipotesi aggiungiIpotesi(MappaturaParziale map) {
+        Ipotesi ip = new Ipotesi(map, this);
+        figli.add(ip);
+        return ip;
+    }
+
+    /*
+     * Ritorna la mappatura completa calcolata risalendo il cammino fino
+     * alla radice, facendo una merge con ogni mappatura incontrata
+     */
+    public MappaturaParziale getStato() {
+        if( null == padre) {
+            return new MappaturaParziale(map);
+        }
+        return map.merge(padre.getStato());
+    }
+
+    /*
+     * Percorrendo il cammino fino alla radice, restituisce il primo nodo
+     * in cui vi è un assegnazione in conflitto con la mappatura m
+     */
+    public Ipotesi trovaConflitto(MappaturaParziale m) {
+        if(map.conflitto(m)) {
+            return this;
+        }
+        if(null == padre) {
+            return null;
+        }
+        return padre.trovaConflitto(m);
+    }
+
+    public void stampa(int d, Ipotesi ipotesiCorrente) {
+        System.out.print("  ");
+        for(int i = 0; i <= d; ++i) {
+            System.out.print("     ");
+        }
+        if(this == ipotesiCorrente) {
+            System.out.print("\b\b**");
+
+        }
+        System.out.println(map);
+
+        for(Ipotesi i : figli) {
+            if(i!= null) {
+                i.stampa(d + 1, ipotesiCorrente);
             }
         }
+    }
 
-    Ipotesi giaRaggiunta(MappaturaParziale mappaturaCorrente) {
+    /*
+     * Esplorando ricorsivamente i nodi figli, restituisce, se esiste, il primo
+     * in cui lo stato sia uguale a m; null altrimenti
+     */
+    Ipotesi giaRaggiunta(MappaturaParziale m) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
